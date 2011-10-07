344d940d8bed2b6f3d436783d82b8bb364aa1d74
Dmytro Golub <dmytro.golub@gmail.com>
2011-10-07 23:10:43 +0100

    Initial Commit

---
+++ ParameterizedBuild/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ParameterizedBuild
+//
+//  Created by Dmytro Golub on 10/7/11.
+//  Copyright 2011 Golub. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
